2111e115b97f77c08ec802fc5d6eee888f90babe
vadlamudi Adarsa Naga Tulasi <161280556+Adarsa9119@users.noreply.github.com>
2025-08-24 14:09:55 +0530

    Add files via upload

---
+++ C/🟢 Beginner Patterns/9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf(" Right-Aligned Triangle\n\n");
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++) printf(" ");
+        for(int k=1;k<=i;k++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
