c4b37efbebc19654088cfa2ea6b06cab8bed7fd2
vadlamudi Adarsa Naga Tulasi <161280556+Adarsa9119@users.noreply.github.com>
2025-08-24 14:23:22 +0530

    Add files via upload

---
+++ C/▶️ Beginner Patterns/2.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int i,j;
+    printf("Hollow Rectangle\n\n");
+    for(i=0;i<4;i++){
+	for(j=0;j<5;j++){
+            if(i==0 || i==3 || j==0 || j==4) printf("* ");
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+OUTPUT:
+* * * * * 
+*       * 
+*       * 
+* * * * * 
