ecede6af1e40f06889e905ea0e3aec3ddaee0eea
vadlamudi Adarsa Naga Tulasi <161280556+Adarsa9119@users.noreply.github.com>
2025-08-24 14:19:06 +0530

    Delete C/🟢 Beginner Patterns directory

--- C/🟢 Beginner Patterns/10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Left-Aligned Triangle\n\n");
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++) printf("*");
-        printf("\n");
-    }
-    return 0;
-}
--- C/🟢 Beginner Patterns/4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf(" Inverted Triangle\n");
-   for(int i=5;i>=1;i--){
-        for(int j=1;j<=i;j++) printf("* ");
-        printf("\n");
-    }
-    return 0;
-}
